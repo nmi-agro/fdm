17c67aa4e5e9d94d488ac871d654de996a6712c6
Mert Bora İnevi <bora.inevi@nmi-agro.nl>
2026-03-18 10:32:24 +0100

    Add cultivation catalogue hash reset to the b_lu_eom_residue renaming migration

--- fdm-core/src/db/migrations/0025_rename-b-lu-eom-residue.sql
+++ fdm-core/src/db/migrations/0025_rename-b-lu-eom-residue.sql
@@ -1,1 +1,2 @@
-ALTER TABLE "fdm"."cultivations_catalogue" RENAME COLUMN "b_lu_eom_residues" TO "b_lu_eom_residue";
+ALTER TABLE "fdm"."cultivations_catalogue" RENAME COLUMN "b_lu_eom_residues" TO "b_lu_eom_residue";--> statement-breakpoint
+UPDATE "fdm"."cultivations_catalogue" SET "hash" = '0000';
