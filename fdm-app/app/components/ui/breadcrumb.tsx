3fffaedd2426252e40309532ebd8a1905eb3e277
SvenVw <37927107+SvenVw@users.noreply.github.com>
2026-05-13 13:27:41 +0200

    fix: revert ui change

--- fdm-app/app/components/ui/breadcrumb.tsx
+++ fdm-app/app/components/ui/breadcrumb.tsx
@@ -1,5 +1,5 @@
 import { ChevronRight, MoreHorizontal } from "lucide-react"
-import { Slot as SlotPrimitive } from "radix-ui"
+import { Slot } from "radix-ui"
 import * as React from "react"
 
 import { cn } from "~/lib/utils"
@@ -45,7 +45,7 @@ const BreadcrumbLink = React.forwardRef<
         asChild?: boolean
     }
 >(({ asChild, className, ...props }, ref) => {
-    const Comp = asChild ? SlotPrimitive.Slot : "a"
+    const Comp = asChild ? Slot : "a"
 
     return (
         <Comp
