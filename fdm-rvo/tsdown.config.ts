fd65102bf6511a3aec9e37357cda4108e6f8e673
SvenVw <37927107+SvenVw@users.noreply.github.com>
2026-07-14 10:19:01 +0200

    refactor: do no create d.ts files in src/ on build

--- fdm-rvo/tsdown.config.ts
+++ fdm-rvo/tsdown.config.ts
@@ -4,6 +4,7 @@ export default defineConfig({
   entry: ["src/index.ts", "src/types.ts", "src/utils.ts", "src/shapefile.ts"],
   format: "esm",
   outDir: "dist",
+  tsconfig: "./tsconfig.build.json",
   dts: true,
   sourcemap: true,
   target: "node24",
